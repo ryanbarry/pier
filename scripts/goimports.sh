f3a3f3f832f99eed140df63bf3ba3ae23b9a54db
Ryan Barry <rbarry@rubiconproject.com>
2016-02-06 18:10:45 -0800

    make goimports -w work with dockerized executable

--- scripts/goimports.sh
+++ scripts/goimports.sh
@@ -1,3 +1,32 @@
 #! /bin/bash
 
-docker run -i --rm ryanbarry/devgo goimports
+opts=""
+bindflag=""
+
+while getopts "w:" flag; do
+  case $flag in
+    w)
+        if [[ -n $OPTARG ]]; then
+          infilepath=$OPTARG
+          tempfile=$(basename $infilepath)
+          tempfilepath=~/$tempfile
+          if [[ -e $tempfilepath ]]; then
+            echo "error: temp file exists!"
+            exit 2
+          fi
+          cp $infilepath $tempfilepath
+          opts="-w /$tempfile "$opts
+          bindflag="-v $tempfilepath:/$tempfile"
+        fi
+        ;;
+    ?)
+      opts="-$OPTARG "$opts
+      ;;
+  esac
+done
+
+docker run -i --rm $bindflag ryanbarry/devgo goimports $opts
+
+if [[ -e $tempfilepath ]]; then
+  mv $tempfilepath $infilepath
+fi
